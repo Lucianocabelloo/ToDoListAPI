f27ca488d4d6f77305d1f0ddaa9d1084f25f9370
lucianocabelloo <you@example.com>
2024-03-18 09:29:46 -0300

    Add functions

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,4 +1,34 @@
 export const listarTareas = async (req,res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
 
+}
+
+export const crearTareas = async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+export const editarTarea = async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const borrarTarea = async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
 
 }
